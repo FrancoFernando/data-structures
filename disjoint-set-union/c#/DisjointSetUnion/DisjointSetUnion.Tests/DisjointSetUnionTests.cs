4e276c32c0b2d57c979df6650c7e3159ef037e00
Fernando Franco <fernando.franco@varian.com>
2021-07-02 17:14:02 +0200

    add dsu c# with test

---
+++ disjoint-set-union/c#/DisjointSetUnion/DisjointSetUnion.Tests/DisjointSetUnionTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DisjointSetUnion.Tests
+{
+
+    [TestClass]
+    public class DisjointSetUnionTests
+    {
+        [TestMethod]
+        public void InitiallyDisjoint()
+        {
+            DisjointSetUnion dsu = new DisjointSetUnion(10);
+
+            for (int i = 0; i < 10; ++i) {
+                Assert.AreEqual(i, dsu.Find(i));
+            }
+        }
+
+        [TestMethod]
+        public void SimpleUnion()
+        {
+            DisjointSetUnion dsu = new DisjointSetUnion(10);
+
+            dsu.Unite(0, 1);
+            Assert.IsTrue(dsu.Same(0, 1));
+
+            dsu.Unite(2, 3);
+            Assert.IsTrue(dsu.Same(2, 3));
+
+            dsu.Unite(4, 5);
+            Assert.IsTrue(dsu.Same(4, 5));
+        }
+
+        [TestMethod]
+        public void ComplexUnion()
+        {
+            DisjointSetUnion dsu = new DisjointSetUnion(10);
+
+            dsu.Unite(0, 1);
+            dsu.Unite(2, 3);
+            dsu.Unite(5, 6);
+            dsu.Unite(7, 8);
+            dsu.Unite(6, 7);
+            dsu.Unite(0, 2);
+
+            Assert.IsTrue(dsu.Same(0, 1));  
+            Assert.IsTrue(dsu.Same(2, 3));
+            Assert.IsTrue(dsu.Same(0, 2));
+            Assert.IsTrue(dsu.Same(8, 6));
+            Assert.IsFalse(dsu.Same(0, 7));
+
+            dsu.Unite(0, 6);
+            Assert.IsTrue(dsu.Same(0, 7));
+        }
+    }
+}
---
+++ disjoint-set-union/c#/DisjointSetUnion/DisjointSetUnion/DisjointSetUnion.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DisjointSetUnion
+{
+    public class DisjointSetUnion
+    {
+        private readonly List<int> parent = new List<int>();
+        private readonly List<int> size = new List<int>();
+
+        public DisjointSetUnion(int n)
+        {
+            parent.AddRange(Enumerable.Range(0, n));
+            size.AddRange(Enumerable.Repeat(0, n));
+        }
+
+        
+        public int Find(int x) {
+
+            while (parent[x] != x)
+            {
+                x = parent[x];
+                parent[x] = parent[parent[x]];
+            }
+            return parent[x];
+        }
+        
+        /*
+        //Recursive
+        public int Find(int x)
+        {
+
+            if (parent[x] == x) return x;
+            return parent[x] = Find(parent[x]);
+        }
+        */
+
+        public void Unite(int x, int y)
+        {
+
+            int rootX = Find(x);
+            int rootY = Find(y);
+
+            if (rootX == rootY) return;
+
+            if (size[rootX] < size[rootY]) (rootX, rootY) = (rootY, rootX);
+            size[rootX] += size[rootY];
+            parent[rootY] = rootX;
+        }
+
+        public bool Same(int x, int y)
+        {
+            return Find(x) == Find(y);
+        }
+    }
+}
